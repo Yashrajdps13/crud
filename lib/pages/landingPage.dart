698bf79667df839da023a2223b43d28389a7adc9
Qu454r1712 <qu454r1712@gmail.com>
2023-08-03 01:55:00 +0530

    images

--- lib/pages/landingPage.dart
+++ lib/pages/landingPage.dart
@@ -1,6 +1,8 @@
+import 'package:crud/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:crud/routes/mapping.dart';
 import 'package:crud/widgets/drawer.dart';
+import 'package:crud/widgets/category.dart';
 
 class Landing_Page extends StatefulWidget {
   const Landing_Page({super.key});
@@ -8,168 +10,28 @@ class Landing_Page extends StatefulWidget {
   @override
   State<Landing_Page> createState() => _Landing_PageState();
 }
-
 class _Landing_PageState extends State<Landing_Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red[400],
-        elevation: 0.0,
-        title: Row(children: [
-          Padding(padding: EdgeInsets.fromLTRB(99, 0, 0, 0)),
-          Text(
-            'App name',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          SizedBox(width: 10),
-          Image.asset(
-            "images/Batman_Logo.png",
-            fit: BoxFit.contain,
-            height: 40,
-          ),
-        ]),
-      ),
+      appBar:MyAppBar(),
       body: Padding(
         padding: EdgeInsets.all(0.0),
         child: SingleChildScrollView(
           child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, MyRoutes.AI_MLRoute);
-                },
-                child: Stack(children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage("images/aiml.png"),
-                    ),
-                  ),
-                  Container(
-                    height: 158,
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "AI and ML",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  )
-                ]),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, MyRoutes.Web_DevRoute);
-                },
-                child: Stack(children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage("images/web.png"),
-                    ),
-                  ),
-                  Container(
-                    height: 158,
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "Web Development",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  )
-                ]),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, MyRoutes.Android_DevRoute);
-                },
-                child: Stack(children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage("images/app.png"),
-                    ),
-                  ),
-                  Container(
-                    height: 158,
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "Android Development",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  )
-                ]),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, MyRoutes.DSARoute);
-                },
-                child: Stack(children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage("images/dsa.png"),
-                    ),
-                  ),
-                  Container(
-                    height: 158,
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "DSA and Programming",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  )
-                ]),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, MyRoutes.DesigningRoute);
-                },
-                child: Stack(children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage("images/designing.png"),
-                    ),
-                  ),
-                  Container(
-                    height: 158,
-                    padding: EdgeInsets.all(10.0),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "Designing",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                  )
-                ]),
-              ),
+              Domain(imageURL:"images/aiml.png" ,name:"AI and ML",renderpage: MyRoutes.AI_MLRoute,),
+              Domain(name:"Web Development",imageURL: "images/web.png",renderpage: MyRoutes.Web_DevRoute,),
+              Domain(name:"Android (Flutter)",imageURL: "images/flutter.png",renderpage: MyRoutes.FlutterRoute,),
+              Domain(name:"Android (Kotlin)",imageURL: "images/kotlin.png",renderpage: MyRoutes.KotlinRoute,),
+              Domain(name:"DSA and Programming",imageURL: "images/dsa.png",renderpage: MyRoutes.DSARoute,),
+              Domain(name:"Designing",imageURL: "images/designing.png",renderpage: MyRoutes.DesigningRoute,),
+              Domain(name:"Management",imageURL: "images/management.png",renderpage: MyRoutes.ManagementRoute,),
             ],
           ),
         ),
       ),
-      drawer:Drawer(),
+      drawer:MyDrawer(),
     );
   }
 }
--- lib/routes/mapping.dart
+++ lib/routes/mapping.dart
@@ -1,8 +1,11 @@
 class MyRoutes{
   static String landingRoute="/landingPage";
   static String AI_MLRoute="/AI_ML.dart";
-  static String Android_DevRoute="/android_dev.dart";
-  static String DesigningRoute="/designing";
-  static String Web_DevRoute="/Web_dev";
+  static String FlutterRoute="/flutter.dart";
+  static String DesigningRoute="/designing.dart";
+  static String Web_DevRoute="/Web_dev.dart";
   static String DSARoute="/DSA.dart";
+  static String ManagementRoute="/management.dart";
+  static String KotlinRoute="/Kotlin.dart";
+  static String ProfileRoute='/profile.dart';
 }
